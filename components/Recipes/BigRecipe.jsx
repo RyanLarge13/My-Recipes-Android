80b9a732d3c11ef396b406a5dba54fba63ace59e
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-12-19 15:01:11 -0800

    Adding a FlatList compinent for showing ingredients.

--- components/Recipes/BigRecipe.jsx
+++ components/Recipes/BigRecipe.jsx
@@ -1,9 +1,21 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, Pressable, FlatList, StyleSheet } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const BigRecipe = ({ onCloseClick, recipe }) => {
   return (
     <View style={styles.container}>
+      <Pressable style={styles.close} onPress={() => onCloseClick()}>
+        <Icon name="close" />
+      </Pressable>
       <Text>{recipe.doc.Title}</Text>
+      <Text>{recipe.doc.Desc}</Text>
+      <View>
+        <FlatList
+          data={recipe.doc.Ingredients}
+          renderItem={({ item }) => <Text>{item}</Text>}
+          style={styles.list}
+        />
+      </View>
     </View>
   );
 };
@@ -19,6 +31,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
+  },
+  close: {
+    position: "absolute",
+    top: "5%",
+    left: "7.5%",
+    padding: 5,
+    borderRadius: 50,
+    backgroundColor: "#f77",
+  },
+  list: {
+    maxHeight: 200,
+    width: 150,
+    borderRadius: 50,
+    backgroundColor: "#fff",
+    elevation: 5,
   },
 });
 
